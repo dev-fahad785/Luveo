43e2f4f1e7c9bcef6c10dae887064d97426313ba
dev-rfgul <rfgul587@gmail.com>
2026-03-12 22:53:39 +0500

    changed the multer imgs size

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // Limit to 5MB
+    limits: { fileSize: 50 * 1024 * 1024 }, // Limit to 5MB
     fileFilter: (req, file, cb) => {
         if (file.mimetype.startsWith('image/')) {
             cb(null, true);
